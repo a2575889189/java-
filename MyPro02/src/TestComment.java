8a510d197cd33b348ffd1029582dba3588c0b8f9
lxc <2575889189@qq.com>
2019-03-22 14:18:21 +0800

    测试四

--- MyPro02/src/TestComment.java
+++ MyPro02/src/TestComment.java
@@ -11,7 +11,8 @@ public class TestComment {
 	 * 这是程序的入口
 	 * @param args 参数
 	 */
-	//修改测试33333
+	//修改测试3333344
+	
 	public static void main(String[] args/* args是参数名，可以更改*/ ) {
 		System.out.println("测试注释！");		//这是打印语句...
 	}
